2a775bf4b9447eceae8fad81c0e19b605c8b2390
Nuno Ribeiro <nunocpr@ua.pt>
2023-05-07 21:23:34 +0100

    fix: show images from DB

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="css/index.css">
     <title>Chat PHP - Mapa</title>
 </head>
+
 <body>
     <h1>Mapa</h1>
     <div class="map-container">
@@ -18,13 +20,13 @@
         // $database = 'deca_23_BDTSS_99';
 
         $host = 'labmm.clients.ua.pt';
-        $username = 'deca_23_BDTSS_99_dbo';
+        $username = 'deca_23_BDTSS_99_web';
         $password = 'CaM3D26d';
         $database = 'deca_23_bdtss_99';
 
         $connection = mysqli_connect($host, $username, $password, $database);
 
-        $query = "SELECT order FROM tiles";
+        $query = "SELECT `order` FROM `tiles`";
         $result = mysqli_query($connection, $query);
 
         if (mysqli_connect_errno()) {
@@ -34,14 +36,14 @@
 
         if ($result) {
             while ($row = $result->fetch_object()) {
-                $number = $row->image_order;
+                $number = $row->order;
                 $imageName = '0' . $number . ".png";
                 echo "<img src=\"imageonline/$imageName\" alt=\"Image\">";
             }
         } else {
             echo "Error retrieving image order from the database: " . mysqli_error($connection);
         }
-        
+
         // $numbers = range(0, 99);
 
         // foreach ($numbers as $number) {
@@ -51,4 +53,5 @@
         ?>
     </div>
 </body>
+
 </html>
