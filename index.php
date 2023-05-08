b82a0d5daeffc0b692b451aff6ef7cdc36184fee
Nuno Ribeiro <nunocpr@ua.pt>
2023-05-08 19:49:49 +0100

    fix: bla

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <?php
 
         // the file path is relative to index.php where this file is included
-        require_once "../basic-chat/connections/connection.php";
+        require_once "../connections/connection.php";
 
         // Create a new DB connection
         $link = new_db_connection();
@@ -26,44 +26,9 @@
         $stmt = mysqli_stmt_init($link);
         $stmt2 = mysqli_stmt_init($link);
 
-        $query = "SELECT `img` FROM `tiles` ORDER BY `order` ASC";
-
-        if (mysqli_stmt_prepare($stmt, $query)) {
-            /* execute the prepared statement */
-            if (mysqli_stmt_execute($stmt)) {
-                /* bind result variables */
-                mysqli_stmt_bind_result($stmt, $img);
-                /* fetch values */
-                while (mysqli_stmt_fetch($stmt)) {
-                    echo "<img src='imageonline/$img' alt='Image $img'>";
-                }
-                $query2 = "SELECT `x_pos` ,`y_pos`  FROM `teams` WHERE `username` = 'PHP Masters'";
-                if (mysqli_stmt_prepare($stmt2, $query2)) {
-                    /* execute the prepared statement */
-                    if (mysqli_stmt_execute($stmt2)) {
-                        /* bind result variables */
-                        mysqli_stmt_bind_result($stmt2, $pos_x, $pos_y);
-                        /* fetch values */
-                        while (mysqli_stmt_fetch($stmt2)) {
-                            echo '<span class="overlay" style="top:'.$pos_y.'px; left:'.$pos_x.'px">X</span>';
-                        }
-                    }
-                }else {
-                    echo "Error:" . mysqli_stmt_error($stmt2);
-                    echo ("Error description: " . mysqli_error($link));
-                }
-            /* close statement */
-            mysqli_stmt_close($stmt);
-            mysqli_stmt_close($stmt2);
-        } else {
-            echo "Error:" . mysqli_stmt_error($stmt);
-            echo ("Error description: " . mysqli_error($link));
-        }
-
+        $query = "";
+        $query2 = "";
         
-        /* close connection */
-        mysqli_close($link);
-    }
         ?>
     </div>
 </body>
